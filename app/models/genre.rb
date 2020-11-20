0c01e9a73b3deee796aa78814513908359e1f606
Michelle Ranly <meranly8@gmail.com>
2020-11-20 03:59:31 +0000

    Automatically backed up by Learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -14,6 +14,8 @@ class Genre < ActiveRecord::Base
 
   def all_artist_names
     # return an array of strings containing every musician's name
-    self.artists.name
+    self.artists.collect do |artist|
+
+    end
   end
 end
